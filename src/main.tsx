b47a0b4a69ef9a404a112117d35d7cf5df205646
JLMCS <chris.johnson@jlmcsfunding.com>
2025-11-05 01:52:01 -0600

    light mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,13 @@ import { ThemeProvider } from "next-themes";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    {/* This restores your original theme setup (light by default) */}
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    {/*
+      THIS IS THE FIX:
+      We are forcing the theme to "light" and removing "enableSystem".
+      This ensures your site is ALWAYS in the original light theme,
+      which will make your localhost match the lovable.dev preview.
+    */}
+    <ThemeProvider attribute="class" defaultTheme="light">
       <App />
     </ThemeProvider>
   </HelmetProvider>
